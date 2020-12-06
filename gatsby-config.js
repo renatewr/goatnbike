ae676a95b8aca394f1235db30278afe8c110919e
Renate Winther Ravnaas <renatewr@gmail.com>
2020-12-06 22:37:44 +0100

    Added gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,31 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Goatnbike',
+    author: 'Renate W Ravnaas',
+    description: 'Goatnbike site, based on Dimension by HTML5 UP',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Goatnbike',
+        short_name: 'goatnbike',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
@@ -19,5 +34,16 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `zysy44islyfs`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        //accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: `e5a7ac785052c6089044743d608d4f53567d3c84bfc90584335266392ac16b5a`,
+      },
+    },
   ],
 }
